0a72b205ede0737bda1db30d8894bc36a1eaa9d8
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-07-15 10:27:47 +1200

    Changed runtools.py

--- csnippex-apizator/runtools.py
+++ csnippex-apizator/runtools.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def main():
-    subprocess.call(['java', '-jar', 'csnippex.jar', '../input-posts', './ser', './maven-jars', 'C:\Program Files\Java\jdk-17.0.2', './tmp', '../output-csnippex'])
+    subprocess.call("csnippex.sh")
 
 
 if __name__ == '__main__':
