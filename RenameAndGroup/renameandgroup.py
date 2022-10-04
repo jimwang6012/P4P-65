09da6fe4c8a588214b4f0de6dc5bc438860a01ea
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-10-05 03:17:34 +1300

    Changed script to generate the proper file structure

--- RenameAndGroup/renameandgroup.py
+++ RenameAndGroup/renameandgroup.py
@@ -1,21 +1,49 @@
 import signal
+import sqlite3
 import subprocess
 import os
 import os.path
 
 
+def create_connection(db_file):
+    """
+    Create a database connection to the SQLite database specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    connection = None
+    try:
+        connection = sqlite3.connect(db_file)
+    except Exception as e:
+        print(e)
+
+    return connection
+
+
 def main():
+
+    # dictionary to store each postId with parentId
+    # dictionary[postId] = parentId
+    dictionary = {}
+    database = "../csnippex-apizator/id-matching.db"
+    connection = create_connection(database)
+
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM post_ids")
+        for row in cursor:
+            dictionary[row[0]] = row[1]
 
     # run rename-class on apizator output .java files with 5 seconds timeout
     arr = os.listdir('../csnippex-apizator/output-apizator-java')
-    num = 0
     for file in arr:
-        num += 1
         try:
             print('Renaming ' + file)
-            os.makedirs('../csnippex-apizator/hi/' + str(num), exist_ok=True)
+            post_id = file.split('.')[0]
+
+            os.makedirs(f'../csnippex-apizator/{dictionary[post_id]}/{post_id}/com/stackoverflow/api', exist_ok=True)
             p = subprocess.Popen(['java', '-jar', 'rename-class.jar', '../csnippex-apizator/output-apizator-java/' + file,
-                                  '../csnippex-apizator/output-csnippex-java/' + file, '../csnippex-apizator/hi/' + str(num) + '/'], start_new_session=True)
+                                  '../csnippex-apizator/output-csnippex-java/' + file, f'../csnippex-apizator/{dictionary[post_id]}/{post_id}/com/stackoverflow/api/'], start_new_session=True)
             p.wait(timeout=5)
         except subprocess.CalledProcessError as e:
             print(e)
