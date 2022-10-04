c54591c08afb97a7d4fc7872d06a77798a3eafa4
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-10-05 03:27:13 +1300

    Fixed other KeyErrors

--- RenameAndGroup/renameandgroup.py
+++ RenameAndGroup/renameandgroup.py
@@ -43,7 +43,7 @@ def main():
 
             os.makedirs(f'../csnippex-apizator/output-rename/{str(dictionary[int(post_id)])}/{post_id}/com/stackoverflow/api', exist_ok=True)
             p = subprocess.Popen(['java', '-jar', 'rename-class.jar', '../csnippex-apizator/output-apizator-java/' + file,
-                                  '../csnippex-apizator/output-csnippex-java/' + file, f'../csnippex-apizator/output-rename/{dictionary[post_id]}/{post_id}/com/stackoverflow/api/'], start_new_session=True)
+                                  '../csnippex-apizator/output-csnippex-java/' + file, f'../csnippex-apizator/output-rename/{str(dictionary[int(post_id)])}/{post_id}/com/stackoverflow/api/'], start_new_session=True)
             p.wait(timeout=5)
         except subprocess.CalledProcessError as e:
             print(e)
