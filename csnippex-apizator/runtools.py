2d34704c7ea47e9720ff46ea33846ff7a722a54b
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-10-04 16:34:30 +1300

    Added process group pruning to prevent memory leak

--- csnippex-apizator/runtools.py
+++ csnippex-apizator/runtools.py
@@ -1,53 +1,70 @@
+import json
 import signal
 import subprocess
 import os
 import os.path
-from subprocess import Popen, PIPE
 import sys
-import json
 
 
 def main():
 
-    # run csnippex on input posts with 10 seconds timeout per post
+    # run csnippex on input posts with 5 seconds timeout per post
     arr = os.listdir('input-posts')
     for file in arr:
         try:
-            print('processing ' + file)
-            p = subprocess.Popen(['java', '-jar', 'csnippex.jar', './input-posts/' + file, './ser', './maven-jars', './jdks/jdk-17.0.2.jdk', './tmp', './output-csnippex'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
+            print('Processing ' + file)
+            p = subprocess.Popen(['java', '-jar', 'csnippex.jar', './input-posts/' + file, './ser', './maven-jars', './jdks/jdk1.8.0_121.jdk', './tmp', './output-csnippex'],
+                                 stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
             p.wait(timeout=5)
         except subprocess.CalledProcessError as e:
             print(e)
         except subprocess.TimeoutExpired:
             print(file + ' timed out')
             os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-    print('finished generating csnippex output')
+    print('Finished generating csnippex output')
 
-    # run apizator on output of csnippex with 10 seconds timeout per post
-    # arr = os.listdir('output-csnippex')
-    # for file in arr:
-    #     try:
-    #         print('processing ' + file)
-    #         subprocess.run('java -jar apizator.jar ./output-csnippex/' + file +
-    #                        ' ./output-apizator ./maven-jars', check=True, timeout=5, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    #     except subprocess.CalledProcessError as e:
-    #         print(e)
-    #     except subprocess.TimeoutExpired:
-    #         print(file + ' timed out')
-    # print('finished generating apizator output')
+    # run apizator on output of csnippex with 5 seconds timeout per post
+    arr = os.listdir('output-csnippex')
+    for file in arr:
+        try:
+            print('Processing ' + file)
+            p = subprocess.Popen(['java', '-jar', 'apizator.jar', './output-csnippex/' + file, './output-apizator', './maven-jars'],
+                                 stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
+            p.wait(timeout=5)
+        except subprocess.CalledProcessError as e:
+            print(e)
+        except subprocess.TimeoutExpired:
+            print(file + ' timed out')
+            os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+    print('Finished generating apizator output')
 
-    # convert the output of apizator into java classes and then compile the classes
-    # arr = os.listdir('output-apizator')
-    # for file in arr:
-    #     f = open("output-apizator/" + file, "r")
-    #     data = json.load(f)
-    #     # create .java files in new directory
-    #     os.makedirs('./com/stackoverflow/api', exist_ok=True)
-    #     f = open('./com/stackoverflow/api/' +
-    #              data['className'] + '.java', "w")
-    #     f.write(
-    #         data['code'])
-    #     f.close()
+    # convert the output of csnippex into java classes
+    arr = os.listdir('output-csnippex')
+    for file in arr:
+        f = open("output-csnippex/" + file, "r")
+        data = json.load(f)
+        # create .java files in new directory
+        os.makedirs('./output-csnippex-java', exist_ok=True)
+        f = open('./output-csnippex-java/' +
+                 data['postId'] + '.java', "w")
+        f.write(
+            data['body'])
+        f.close()
+    print('Finished converting csnippex output to java files')
+
+    # convert the output of apizator into java classes
+    arr = os.listdir('output-apizator')
+    for file in arr:
+        f = open("output-apizator/" + file, "r")
+        data = json.load(f)
+        # create .java files in new directory
+        os.makedirs('./output-apizator-java', exist_ok=True)
+        f = open('./output-apizator-java/' +
+                 data['postId'] + '.java', "w")
+        f.write(
+            data['code'])
+        f.close()
+    print('Finished converting apizator output to java files')
     #     try:
     #         # compile the .java files
     #         subprocess.run("javac ./com/stackoverflow/api/" +
