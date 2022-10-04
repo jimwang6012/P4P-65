67b1ace014f089c5aa1b3d03de82c9faba4cb839
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-10-05 03:20:00 +1300

    Added output-rename folder

--- RenameAndGroup/renameandgroup.py
+++ RenameAndGroup/renameandgroup.py
@@ -41,9 +41,9 @@ def main():
             print('Renaming ' + file)
             post_id = file.split('.')[0]
 
-            os.makedirs(f'../csnippex-apizator/{dictionary[post_id]}/{post_id}/com/stackoverflow/api', exist_ok=True)
+            os.makedirs(f'../csnippex-apizator/output-rename/{dictionary[post_id]}/{post_id}/com/stackoverflow/api', exist_ok=True)
             p = subprocess.Popen(['java', '-jar', 'rename-class.jar', '../csnippex-apizator/output-apizator-java/' + file,
-                                  '../csnippex-apizator/output-csnippex-java/' + file, f'../csnippex-apizator/{dictionary[post_id]}/{post_id}/com/stackoverflow/api/'], start_new_session=True)
+                                  '../csnippex-apizator/output-csnippex-java/' + file, f'../csnippex-apizator/output-rename/{dictionary[post_id]}/{post_id}/com/stackoverflow/api/'], start_new_session=True)
             p.wait(timeout=5)
         except subprocess.CalledProcessError as e:
             print(e)
