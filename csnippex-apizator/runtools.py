49352ec95a9dce36b9b4dc4b843aaed84929d9f7
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-10-04 17:57:58 +1300

    Re-enabled csnippex and apizator scripts

--- csnippex-apizator/runtools.py
+++ csnippex-apizator/runtools.py
@@ -8,35 +8,35 @@ import sys
 
 def main():
 
-    # # run csnippex on input posts with 5 seconds timeout per post
-    # arr = os.listdir('input-posts')
-    # for file in arr:
-    #     try:
-    #         print('Processing ' + file)
-    #         p = subprocess.Popen(['java', '-jar', 'csnippex.jar', './input-posts/' + file, './ser', './maven-jars', './jdks/jdk1.8.0_121.jdk', './tmp', './output-csnippex'],
-    #                              stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
-    #         p.wait(timeout=5)
-    #     except subprocess.CalledProcessError as e:
-    #         print(e)
-    #     except subprocess.TimeoutExpired:
-    #         print(file + ' timed out')
-    #         os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-    # print('Finished generating csnippex output')
+    # run csnippex on input posts with 5 seconds timeout per post
+    arr = os.listdir('input-posts')
+    for file in arr:
+        try:
+            print('Processing ' + file)
+            p = subprocess.Popen(['java', '-jar', 'csnippex.jar', './input-posts/' + file, './ser', './maven-jars', './jdks/jdk1.8.0_121.jdk', './tmp', './output-csnippex'],
+                                 stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
+            p.wait(timeout=5)
+        except subprocess.CalledProcessError as e:
+            print(e)
+        except subprocess.TimeoutExpired:
+            print(file + ' timed out')
+            os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+    print('Finished generating csnippex output')
 
-    # # run apizator on output of csnippex with 5 seconds timeout per post
-    # arr = os.listdir('output-csnippex')
-    # for file in arr:
-    #     try:
-    #         print('Processing ' + file)
-    #         p = subprocess.Popen(['java', '-jar', 'apizator.jar', './output-csnippex/' + file, './output-apizator', './maven-jars'],
-    #                              stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
-    #         p.wait(timeout=5)
-    #     except subprocess.CalledProcessError as e:
-    #         print(e)
-    #     except subprocess.TimeoutExpired:
-    #         print(file + ' timed out')
-    #         os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-    # print('Finished generating apizator output')
+    # run apizator on output of csnippex with 5 seconds timeout per post
+    arr = os.listdir('output-csnippex')
+    for file in arr:
+        try:
+            print('Processing ' + file)
+            p = subprocess.Popen(['java', '-jar', 'apizator.jar', './output-csnippex/' + file, './output-apizator', './maven-jars'],
+                                 stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
+            p.wait(timeout=5)
+        except subprocess.CalledProcessError as e:
+            print(e)
+        except subprocess.TimeoutExpired:
+            print(file + ' timed out')
+            os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+    print('Finished generating apizator output')
 
     # convert the output of csnippex into java classes
     arr = os.listdir('output-csnippex')
@@ -70,6 +70,7 @@ def main():
                 data['code'])
             f.close()
     print('Finished converting apizator output to java files')
+
     #     try:
     #         # compile the .java files
     #         subprocess.run("javac ./com/stackoverflow/api/" +
