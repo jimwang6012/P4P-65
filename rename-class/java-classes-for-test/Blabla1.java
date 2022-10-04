ce3143e6b79cc5b2320ba3af5310c6c47610212f
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-10-04 16:37:10 +1300

    Created rename-class.jar utility using JavaParser

---
+++ rename-class/java-classes-for-test/Blabla1.java
@@ -0,0 +1,12 @@
+package com.stackoverflow.rename_class;
+
+public class Blabla {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+
+    public static void xxx() {
+        Blabla yyy = new Blabla();
+    }
+}
