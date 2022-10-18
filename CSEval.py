2c073efc850a2f3ff67630fff74c3b4aff1d02b0
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-10-19 04:39:25 +1300

    Added working directory change to run all other scripts

--- CSEval.py
+++ CSEval.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from DeleteNotRelatedPosts import deletenotrelatedposts
 from JSONGenerator import jsongenerator
@@ -20,35 +21,55 @@ def main():
         print('You need to specify the module to run')
         sys.exit()
 
+    cwd = os.getcwd()
+
     flag = sys.argv[1]
 
     if flag == '-parse-posts' or flag == '-parse-and-all':
+        os.chdir(cwd)
+        os.chdir('./PostParser')
         postparser.main()
 
     if flag == '-filter-java' or flag == '-parse-and-all':
+        os.chdir(cwd)
+        os.chdir('./DeleteNotRelatedPosts')
         deletenotrelatedposts.main()
 
     if flag == '-generate-json' or flag == '-parse-and-all':
+        os.chdir(cwd)
+        os.chdir('./JSONGenerator')
         jsongenerator.main()
 
     if flag == '-csnippex-apizator' or flag == '-all' or flag == '-parse-and-all':
+        os.chdir(cwd)
+        os.chdir('./csnippex-apizator')
         csnippex_apizator.main()
 
     if flag == '-group-answers' or flag == '-all' or flag == '-parse-and-all':
+        os.chdir(cwd)
+        os.chdir('./RenameAndGroup')
         rename_and_group.main()
 
     if flag == '-generate-run-tests' or flag == '-all' or flag == '-parse-and-all':
+        os.chdir(cwd)
+        os.chdir('./GenerateAndRunTests')
         generateandrun.main()
 
     if flag == '-analyse-answers' or flag == '-all' or flag == '-parse-and-all':
+        os.chdir(cwd)
+        os.chdir('./GenerateAndRunTests')
         executiontime.main()
         executiontimeanalysis.main()
         staticanalysis.main()
 
     if flag == '-generate-analysis' or flag == '-all':
+        os.chdir(cwd)
+        os.chdir('./AnalyseResults')
         generateanalysis.main()
 
     if flag == 'list-groups':
+        os.chdir(cwd)
+        os.chdir('./GenerateAndRunTests')
         listgroups.main()
 
 
