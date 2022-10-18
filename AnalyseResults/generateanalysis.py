6ea86811604d206b08bcbeb3ed9df8d372c96b56
Jimmy Wang <swan825@aucklanduni.ac.nz>
2022-10-19 03:21:42 +1300

    Added outlier detection and additional metric measurements to the table

--- AnalyseResults/generateanalysis.py
+++ AnalyseResults/generateanalysis.py
@@ -1,6 +1,18 @@
 import os
 import os.path
 from tabulate import tabulate
+import numpy as np
+
+
+def average(arr):
+    return sum(arr) / len(arr)
+
+
+def inter_quartile_range(arr):
+    data = np.array(arr)
+    q3, q1 = np.percentile(data, [75, 25])
+    iqr = q3 - q1
+    return q1, q3, iqr
 
 
 def main():
@@ -20,14 +32,41 @@ def main():
                     header = ["Answers"]
                     test_order = []
                     results_dict = {}
+                    confidence_dict = {}
+                    exec_time_dict = {}
+                    warning_count_dict = {}
+                    correctness_outliers = []
+                    exec_time_outliers = []
                     for temp_file in arr:
-                        results_dict[temp_file] = []
+                        if temp_file != "AnalysisTable.txt":
+                            results_dict[temp_file] = []
+                            confidence_dict[temp_file] = 0.0
+                            exec_time_dict[temp_file] = 0.0
+                            warning_count_dict[temp_file] = 0
                     for file3 in arr:
                         if file3 != "AnalysisTable.txt":
                             temp_test_order = []
                             test_count = 0
                             results = []
                             confidence_list = []
+                            avg_exec_time = 0.0
+                            warning_count = 0
+                            try:
+                                with open(
+                                        '../GenerateAndRunTests/output-rename/' + file1 + '/groups/' + file2 + '/' + file3 +
+                                        '/com/stackoverflow/api/' + file3 + 'ExecutionTimeAnalysis.txt', 'r') as f:
+                                    avg_exec_time = round(float(f.readline()), 2)
+                            except FileNotFoundError as e:
+                                print(e)
+
+                            try:
+                                with open(
+                                        '../GenerateAndRunTests/output-rename/' + file1 + '/groups/' + file2 + '/' + file3 +
+                                        '/com/stackoverflow/api/' + file3 + 'StaticAnalysisParsing.txt', 'r') as f:
+                                    warning_count = int(f.readline())
+                            except FileNotFoundError as e:
+                                print(e)
+
                             try:
                                 with open(
                                         '../GenerateAndRunTests/output-rename/' + file1 + '/groups/' + file2 + '/' + file3 +
@@ -50,6 +89,7 @@ def main():
                                             passed = int(line.split('OK (')[1].split(' test')[0])
                                             test_count += passed
                                             results.append(f"P:{str(passed)} F:0 100%")
+                                            confidence_list.append(100.0)
                                         if line.startswith('Tests run:'):
                                             line_data = line.split('Tests run: ')[1].split(',  Failures: ')
                                             passed = int(line_data[0])
@@ -59,12 +99,16 @@ def main():
                                             test_count += failed
                                             results.append(
                                                 f"P:{str(passed)} F:{str(failed)} {str(round(confidence, 2))}%")
+                                            confidence_list.append(confidence)
                             except FileNotFoundError as e:
                                 print(e)
                             if len(temp_test_order) > len(test_order):
                                 test_order = temp_test_order
                             if test_count != 0:
                                 results_dict[file3] = results
+                                confidence_dict[file3] = round(average(confidence_list), 2)
+                                exec_time_dict[file3] = avg_exec_time
+                                warning_count_dict[file3] = warning_count
 
                     is_empty = True
                     for v in results_dict.values():
@@ -72,18 +116,52 @@ def main():
                             is_empty = False
 
                     if not is_empty:
+                        correctness_group = []
+                        exec_time_group = []
+                        for test_id in test_order:
+                            correctness_group.append(confidence_dict[test_id])
+                            exec_time_group.append(exec_time_dict[test_id])
+                        correctness_q1, correctness_q3, correctness_iqr = inter_quartile_range(correctness_group)
+                        exec_time_q1, exec_time_q3, exec_time_iqr = inter_quartile_range(exec_time_group)
+
                         header += test_order
+                        header.append("AVG Correctness")
+                        header.append("AVG Execution Time")
+                        header.append("PMD Violations")
                         table_results = []
                         for test_id in test_order:
                             if len(results_dict[test_id]) != 0:
-                                table_results.append([test_id] + results_dict[test_id])
 
-                        print(title)
-                        print(tabulate(table_results, headers=header, tablefmt="fancy_grid"))
+                                if confidence_dict[test_id] < (correctness_q1 - 1.5 * correctness_iqr):
+                                    correctness_outliers.append(test_id)
+                                if exec_time_dict[test_id] > (exec_time_q3 + 1.5 * exec_time_iqr):
+                                    exec_time_outliers.append(test_id)
 
-                    with open('../GenerateAndRunTests/output-rename/' +
-                              file1 + '/groups/' + file2 + '/AnalysisTable.txt', 'w') as outfile:
-                        outfile.writelines([title + '\n', tabulate(table_results, headers=header, tablefmt="grid")])
+                                table_results.append([test_id] + results_dict[test_id] +
+                                                     [confidence_dict[test_id]] +
+                                                     [exec_time_dict[test_id]] + [warning_count_dict[test_id]])
+
+                        with open('../GenerateAndRunTests/output-rename/' +
+                                  file1 + '/groups/' + file2 + '/AnalysisTable.txt', 'w') as outfile:
+
+                            print(title)
+                            print(tabulate(table_results, headers=header, tablefmt="fancy_grid"))
+
+                            if len(correctness_outliers) == 0:
+                                correctness_outliers_string = "Correctness Outliers: None"
+                            else:
+                                correctness_outliers_string = "Correctness Outliers: " + ", ".join(correctness_outliers)
+                            if len(exec_time_outliers) == 0:
+                                exec_time_outliers_string = "Execution Time Outliers: None"
+                            else:
+                                exec_time_outliers_string = "Execution Time Outliers: " + ", ".join(exec_time_outliers)
+
+                            print(correctness_outliers_string)
+                            print(exec_time_outliers_string)
+                            print()
+
+                            outfile.writelines([title + '\n', tabulate(table_results, headers=header, tablefmt="grid"),
+                                                '\n' + correctness_outliers_string, '\n' + exec_time_outliers_string, '\n'])
 
 
 if __name__ == '__main__':
